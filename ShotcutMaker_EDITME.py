66f5c654a7aa4ce3af7adda90b885277a59903b6
MrFums <57535680+MrFums@users.noreply.github.com>
2020-10-27 17:22:19 +0000

    Update ShotcutMaker_EDITME.py

--- ShotcutMaker_EDITME.py
+++ ShotcutMaker_EDITME.py
@@ -11,7 +11,7 @@ from win32com.client import Dispatch
 
 
 desktop = winshell.desktop()
-path = os.path.join(desktop, "Valorant Bot.lnk") #name of the shortcut, not needed to be edited
+path = os.path.join(desktop, "Valbot.lnk") #name of the shortcut, not needed to be edited
 target = ("C:\\ValorantXPBot\\bot.py") # where the python script is, change it to where your bot.py is located
 wDir = ("C:\\ValorantXPBot\\") #The folder directory / working directory, change it to where your install folder is located
 icon = ("C:\\ValorantXPBot\\icon.ico")# where the shortcut icon is, change it to where your icon.ico is located (same as bot.py)
