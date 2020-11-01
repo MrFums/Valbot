b40fe68af0a0cffcfe9e01b45a2472961d679796
MrFums <57535680+MrFums@users.noreply.github.com>
2020-11-01 13:54:45 +0000

    Updated

--- ShortcutMaker_EDITME.py
+++ ShortcutMaker_EDITME.py
@@ -12,9 +12,9 @@ from win32com.client import Dispatch
 
 desktop = winshell.desktop()
 path = os.path.join(desktop, "Valbot.lnk") #name of the shortcut, not needed to be edited
-target = ("C:\\ValorantXPBot\\bot.py") # where the python script is, change it to where your bot.py is located
-wDir = ("C:\\ValorantXPBot\\") #The folder directory / working directory, change it to where your install folder is located
-icon = ("C:\\ValorantXPBot\\icon.ico")# where the shortcut icon is, change it to where your icon.ico is located (same as bot.py)
+target = ("C:\\Valbot\\bot.py") # where the python script is, change it to where your bot.py is located
+wDir = ("C:\\Valbot\\") #The folder directory / working directory, change it to where your install folder is located
+icon = ("C:\\Valbot\\icon.ico")# where the shortcut icon is, change it to where your icon.ico is located (same as bot.py)
 
 
 
@@ -25,5 +25,3 @@ shortcut.Targetpath = target
 shortcut.WorkingDirectory = wDir
 shortcut.IconLocation = icon
 shortcut.save()
-
-
