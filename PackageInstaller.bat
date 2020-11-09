09b004cfea67c2b7108cd9467175c7dcdcc813f8
MrFums <57535680+MrFums@users.noreply.github.com>
2020-11-09 02:40:15 +0000

    1.7.4

---
+++ PackageInstaller.bat
@@ -0,0 +1,11 @@
+py -m pip install --upgrade pip
+py -m pip install pillow==8.0.1
+py -m pip install pyautogui==0.9.52
+py -m pip install opencv-python==4.4.0.46
+py -m pip install psutil==5.7.3
+py -m pip install colorama==0.4.4
+py -m pip install winshell==0.6
+py -m pip install pypiwin32==223
+py -m pip install discord_webhook==0.11.0
+py -m pip install pypresence==4.0.0
+py -m pip install numpy==1.19.3
--- main.py
+++ main.py
@@ -2,80 +2,112 @@ import math
 import os
 import time
 from pathlib import Path
-
+import requests
 from colorama import Fore, Style
 from colorama import init
+from pypresence import Presence
 
 init()
-
+os.system('mode con: cols=39 lines=25')
 
 def main():
-    print(Style.BRIGHT + Fore.RED + """
-                                   
-                                                                        
-    8b           d8              88  88                                 
-    `8b         d8'              88  88                          ,d     
-     `8b       d8'               88  88                          88     
-      `8b     d8'    ,adPPYYba,  88  88,dPPYba,    ,adPPYba,   MM88MMM  
-       `8b   d8'     ""     `Y8  88  88P'    "8a  a8"     "8a    88     
-        `8b d8'      ,adPPPPP88  88  88       d8  8b       d8    88     
-         `888'       88,    ,88  88  88b,   ,a8"  "8a,   ,a8"    88,    
-          `8'        `"8bbdP"Y8  88  8Y"Ybbd8"'    `"YbbdP"'     "Y888  
-                                                                         
-                                                                        """)
+    
+    try:
+        RPC = Presence(client_id="772841390467711041")
+        RPC.connect()
+    except Exception:
+        pass
+    
+    version = "Valbot v1.7.4"
+    versionstripped = version.replace("Valbot ", "")
+
+
+    activeactivity = "In console"
+
+    try:
+
+        RPC.update(state=("Running "+ version.replace("Valbot ","")), start=time.time(), large_image="valbot",large_text=version, details=activeactivity)
+
+    except Exception:
+        pass
+
+    print(Fore.RED + """
+    ╔╗  ╔╗╔═══╗╔╗   ╔══╗ ╔═══╗╔════╗
+    ║╚╗╔╝║║╔═╗║║║   ║╔╗║ ║╔═╗║║╔╗╔╗║
+    ╚╗║║╔╝║║ ║║║║   ║╚╝╚╗║║ ║║╚╝║║╚╝
+     ║╚╝║ ║╚═╝║║║ ╔╗║╔═╗║║║ ║║  ║║  
+     ╚╗╔╝ ║╔═╗║║╚═╝║║╚═╝║║╚═╝║  ║║  
+      ╚╝  ╚╝ ╚╝╚═══╝╚═══╝╚═══╝  ╚╝
+                             """ + Style.NORMAL + Fore.RED, versionstripped + Style.RESET_ALL)
 
     print(Style.RESET_ALL)
-    print(Fore.RED + "                         v1.7.3" + Style.RESET_ALL, "-" + Fore.RED,
-          Style.BRIGHT + "by Fums and WolfAnto")
-    print(
-        Style.RESET_ALL + Fore.RED + "———————————————————————————————————————————————————————————————————————————————")
+    print(Style.RESET_ALL + Fore.RED + "          By Fums & WolfAnto")
+    print(Style.RESET_ALL)
+    print(Style.RESET_ALL + Fore.RED + "———————————————————————————————————————")
 
-    print(Style.RESET_ALL + Style.BRIGHT + Fore.RED)
-    print(Fore.WHITE + " 1)", Fore.YELLOW + "Start Bot")
-    print("")
-    print(Fore.WHITE + " 2)", Fore.YELLOW + "Information")
-    print("")
-    print(Fore.WHITE + " 3)", Fore.YELLOW + "Manage Discord Webhook")
-    print("")
-    print(Fore.WHITE + " 4)", Fore.YELLOW + "XP Calculator")
-    print("")
-    print(Fore.WHITE + " 5)", Fore.YELLOW + "Restart Bot")
-    print("")
-    print(Fore.WHITE + " 6)", Fore.YELLOW + "Exit Bot")
-    print("")
-    print(Style.BRIGHT + Fore.GREEN + "")
+    print(Style.RESET_ALL)
+    print(Fore.RED + " [1]", Style.BRIGHT + "Start Bot")
+    print(Style.RESET_ALL)
+    print(Fore.RED + " [2]", Style.BRIGHT + "Information")
+    print(Style.RESET_ALL)
+    print(Fore.RED + " [3]", Style.BRIGHT + "Manage Discord Webhook")
+    print(Style.RESET_ALL)
+    print(Fore.RED + " [4]", Style.BRIGHT + "XP Calculator")
+    print(Style.RESET_ALL)
+    print(Fore.RED + " [5]", Style.BRIGHT + "Exit Bot")
+    print(Style.RESET_ALL)
 
+    print(Fore.YELLOW + "")
     menu = int(input(" > "))
 
     try:
         if menu == 1:
 
             print(Style.RESET_ALL)
-            time.sleep(2)
-            print(Fore.WHITE + " 1)", Fore.CYAN + "Make sure Valorant is focused and make sure you are in the",
-                  Fore.BLUE + "MENU", Fore.CYAN, Style.RESET_ALL)
-            print("")
-            print(Fore.WHITE + " 2)", Fore.CYAN + "Bot will then be started in", Fore.BLUE + "10 seconds",
-                  Fore.CYAN + Style.RESET_ALL)
-            print("")
-            print(Fore.WHITE + " 3)",
-                  Fore.CYAN + "The bot should now run by itself, do not interrupt it unless you want to stop the bot",
-                  Style.RESET_ALL)
+            print(Fore.YELLOW, "Make sure you have the")
+            print(Style.BRIGHT, "Valorant Card", Style.NORMAL + "Player Card equipped")
             print(Style.RESET_ALL)
-            print("")
+            print(Fore.YELLOW, "Valorant must be" + Style.BRIGHT, "FULLSCREEN")
+            print(Style.NORMAL, "for the bot to function")
+            response = requests.get("https://api.github.com/repos/MrFums/Valbot/releases/latest")
+            latest2 = response.json()["name"]
+            changelog = response.json()["body"]
+
+            latest3 = latest2.replace("Valbot v", "")
+            latest4 = latest2.replace("Valbot ","")
+            latest = latest3.replace(".", "")
+            latest = int(latest)
+
+            version = version.replace("Valbot v", "")
+            version = version.replace(".", "")
+            version = int(version)
+
+            if version < latest:
+                print(Style.RESET_ALL)
+                print(Fore.RED, "Version (" + str(versionstripped) + ") is outdated")
+                print(Style.RESET_ALL)
+                print(Fore.RED, "Download latest version (" + str(latest4) + ") now")
+                print(Style.RESET_ALL)
+                time.sleep(5)
+
             print(Style.RESET_ALL)
 
             vallnk = Path("Valorant.lnk")
             if vallnk.is_file():
                 # file exists
-                time.sleep(5)
-
+                try:
+                    RPC.close()
+                except Exception:
+                    pass
+                time.sleep(2)
                 os.startfile("bot.py")
-
+                time.sleep(.5)
+                quit()
             else:
-                print(Fore.RED, 'You do not have a Valorant shortcut named "Valorant" in the bots directory')
+                print(Fore.RED, 'You do not have a Valorant shortcut')
+                print(' named "Valorant" in the directory')
+                print(Fore.RED, 'Add one now to continue')
                 time.sleep(5)
-                print(Fore.RED, 'Create on now; the bot will not continue until you do so')
                 print(Style.RESET_ALL)
                 if os.path.exists("runtime_values"):
                     os.remove("runtime_values")
@@ -85,60 +117,24 @@ def main():
 
         elif menu == 2:
             print(Style.RESET_ALL + "")
-            print(Fore.RED + " HOW TO RUN THE BOT")
+            print(Fore.RED + " IMPORTANT INFORMATION")
             print("")
-            print(Fore.WHITE + " 1)",
-                  Fore.CYAN + "Setup your Valorant shortcut and discord webhook (optional)", Style.RESET_ALL)
+            print(Fore.WHITE + " 1)", Fore.CYAN + "Setup your Valorant shortcut")
+            print("    and discord webhook (optional)", Style.RESET_ALL)
             print("")
-            print(Fore.WHITE + " 2)", Fore.CYAN + "Run the bot by selecting", Fore.BLUE + "1",
-                  Fore.CYAN + "in the main menu")
+            print(Fore.WHITE + " 2)", Fore.CYAN + "Change your player card to the")
+            print("    Valorant Card Player Card")
             print("")
-            print(Fore.WHITE + " 3)", Fore.CYAN + "Change your player card to the",
-                  Fore.BLUE + "Valorant Card Player Card", Fore.CYAN)
+            print(Fore.WHITE + " 3)", Fore.CYAN + "Valorant must be fullscreen")
+            print("    and focused")
             print("")
-            print(Fore.WHITE + " 4)", Fore.CYAN + "Follow the instructions in the ", Fore.BLUE + "bot console",
-                  Fore.CYAN)
-            print("")
-            print(Fore.WHITE + " 5)", Fore.CYAN + "Let the game run and try not to touch your mouse / allow anything")
-            print(Fore.CYAN + "    to interrupt the process")
-            print("")
-            print(Fore.CYAN + " If you wish to stop the bot", Fore.BLUE + "simply close this window", Fore.CYAN)
-            print("")
-            print("")
-            print(Fore.RED + " OTHER PRECAUTIONS AND INFORMATION")
-            print("")
-            print(Fore.CYAN + " After verison 1.6 the bot will now restart itself every 2 hours")
-            print(Fore.CYAN + " You must put a shortcut to Valorant in the same directory of this bot")
-            print(Fore.CYAN + " XP gain is quite slow. This is due to the Deathmatch queue")
-            print(Fore.CYAN + " If you can't find a game, change servers")
-            print(Fore.CYAN + " It is advised to turn the mame hide option and decline friend requests on in",
-                  Fore.BLUE + "Settings > Privacy", Fore.CYAN)
-            print(Fore.CYAN + " Tested only on", Fore.BLUE + "1920x1080", Style.RESET_ALL)
-            print("")
-            print("")
-            print(Fore.RED + " XP GAIN")
-            print("")
-            print(Fore.CYAN + " Average XP gains are as follows")
-            print(Fore.WHITE + " 1 hour:", Fore.MAGENTA + "3,600 XP")
-            print(Fore.WHITE + " 8 hours:", Fore.MAGENTA + "28,800 XP")
-            print(Fore.WHITE + " 24 hours:", Fore.MAGENTA + "86,400 XP")
-            print("")
-            print("")
-            print(Fore.RED + " CREDITS AND CONTACT")
-            print("")
-            print(Fore.CYAN + " Bot is written by and managed by", Fore.BLUE + "Fums",
-                  Fore.CYAN + "to work with Valorant")
-            print(Fore.CYAN + " This version of the bot is maintained by", Fore.BLUE + "Fums")
-            print(Fore.CYAN + " Help will not be given if it isn't regarding the bots code or a bug")
-            print(Fore.CYAN + " If your question / fix is already on the GitHub page, your request may be ignored")
-            print(Fore.BLUE + " Discord :", Fore.YELLOW + "Fums#0888", Fore.CYAN + "| ", Fore.BLUE + "Github :",
-                  Fore.YELLOW + "https://github.com/MrFums", Style.RESET_ALL)
+            print(Fore.BLUE + " Discord :", Fore.YELLOW + "Fums#0888")
+            print(Fore.CYAN, Fore.BLUE + "Github :",Fore.YELLOW + "https://github.com/MrFums", Style.RESET_ALL)
             print(Style.RESET_ALL)
             print(Style.BRIGHT, Fore.RED)
             print("")
-            print(" Input anything to return to the menu...")
-            print(Style.RESET_ALL)
-            print(Style.BRIGHT + Fore.GREEN + "")
+            print(" Input anything to return...")
+            print(Fore.YELLOW, Style.NORMAL + "")
             input(" > ")
             main()
 
@@ -148,27 +144,29 @@ def main():
             print(Style.RESET_ALL)
             print(Fore.RED + " WEBHOOK MANAGER")
             print(Style.RESET_ALL)
-            print(Fore.WHITE + " 1)",
-                  Fore.CYAN + "Open Discord, go to a server where you have permission to create and manage a webhook")
+            print(Fore.WHITE + " 1)", Fore.CYAN + "Open Discord, go to a server where")
+            print("    you have permission to create")
+            print("    and manage a webhook")
             print("")
-            print(Fore.WHITE + " 2)",
-                  Fore.CYAN + "Either create and / or edit an existing channel and go to 'Integrations'")
+            print(Fore.WHITE + " 2)", Fore.CYAN + "Either create and / or edit an ")
+            print('    existing channel and go to')
+            print('    "Integrations"')
             print("")
-            print(Fore.WHITE + " 3)",
-                  Fore.CYAN + "Click webhooks and then click 'Create Webhook', select the right channel,")
-            print(Fore.CYAN, "edit the name and image if you wish, and then click 'Copy Webhook URL'")
+            print(Fore.WHITE + " 3)", Fore.CYAN + "Click webhooks and then click")
+            print('    "Create Webhook", select the right')
+            print('    channel, and then click')
+            print('    "Copy Webhook URL"')
             print("")
-            print(Fore.WHITE + " 4)",
-                  Fore.CYAN + "Paste your webhook below with nothing else, make sure there are no spaces")
+            print(Fore.WHITE + " 4)", Fore.CYAN + "Paste your webhook below")
             print(Style.RESET_ALL)
             print(Style.BRIGHT, Fore.RED)
-            print("")
-            print(" Input anything other than a discord webhook to return to the menu...")
+            print(" Paste a webhook to save or anything")
+            print(" else to cancel")
             print(Style.RESET_ALL)
-            print(Style.BRIGHT + Fore.GREEN + "")
+            print(Style.RESET_ALL)
 
             urlcheck = "https://discord.com/api/webhooks/"
-
+            print(Fore.YELLOW + "")
             inputwebhook = input(" > ")
             inputwebhook = inputwebhook.replace("https://discordapp.com/api/webhooks/",
                                                 "https://discord.com/api/webhooks/")
@@ -196,10 +194,10 @@ def main():
 
         elif menu == 4:
             print(Style.RESET_ALL)
-            print(Style.RESET_ALL, Fore.YELLOW + Style.BRIGHT, "How much XP do you need to get to the item you want?")
+            print(Style.RESET_ALL, Fore.YELLOW + Style.BRIGHT, "How much XP do you need to earn?")
             try:
                 print(Style.RESET_ALL)
-                print(Style.BRIGHT + Fore.GREEN + "")
+                print(Fore.YELLOW + "")
                 xpai = int(input(" > "))
             except ValueError:
                 print(" Error 2: You must enter an integer!")
@@ -209,27 +207,21 @@ def main():
             xpa = xpai / 900
             xpau = (math.ceil(xpa))
             print(Style.RESET_ALL)
-            print(Style.RESET_ALL + Style.BRIGHT + Fore.GREEN, "You need to complete",
-                  Style.RESET_ALL + Fore.GREEN + str(xpau), "deathmatch games", Style.BRIGHT + "to get", xpai, "XP")
-
+            print(Fore.BLUE, "Required Games: " + Style.BRIGHT + str(xpau), Fore.BLUE + "games")
             print(Style.RESET_ALL)
-            print(Style.RESET_ALL + Style.BRIGHT + Fore.GREEN, "This will take roughly",
-                  Style.RESET_ALL + Fore.GREEN + str(xpau / 4), Style.BRIGHT + "hours of bot runtime")
+            print(Fore.BLUE, "ETA:", Style.BRIGHT + str(xpau / 4), Fore.BLUE + "hours")
 
             time.sleep(7)
             print(Style.RESET_ALL)
             main()
 
         elif menu == 5:
-            os.startfile("restart.py")
-
-        elif menu == 6:
-            print(Style.BRIGHT + Fore.RED + " Closing the bot")
+            print(Style.BRIGHT + Fore.RED + " Quitting...")
             time.sleep(1)
             quit()
 
         else:
-            print(" Error 1: Enter a valid integer within the range 1 - 6!")
+            print(" Error 1: Enter a valid integer within the range 1 - 5!")
             time.sleep(2)
             main()
 
