135e78c859f88db2cb1a5d8bc18242d4eebf0c6e
MrFums <57535680+MrFums@users.noreply.github.com>
2021-03-19 23:02:47 +0000

    Update PackageInstaller.bat

--- PackageInstaller.bat
+++ PackageInstaller.bat
@@ -1,12 +1,13 @@
 py -m pip install --upgrade pip
-py -m pip install pillow==8.0.1
-py -m pip install pyautogui==0.9.52
-py -m pip install opencv-python==4.4.0.46
-py -m pip install psutil==5.7.3
-py -m pip install colorama==0.4.4
-py -m pip install winshell==0.6
-py -m pip install pypiwin32==223
-py -m pip install discord_webhook==0.11.0
-py -m pip install pypresence==4.0.0
-py -m pip install numpy==1.19.3
-py -m pip install keyboard==0.13.5
+py -m pip install opencv-python
+py -m pip install pillow
+py -m pip install pyautogui
+py -m pip install psutil
+py -m pip install colorama
+py -m pip install winshell
+py -m pip install pypiwin32
+py -m pip install discord_webhook
+py -m pip install pypresence
+py -m pip install numpy
+py -m pip install keyboard
+py -m pip install requests
