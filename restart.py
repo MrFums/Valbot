b311b3e33b41873d7e45727767b5374a1200fb8d
Fums <57535680+MrFums@users.noreply.github.com>
2021-07-28 17:34:21 +0100

    Delete restart.py

--- restart.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-time.sleep(5)
-os.startfile("bot.py")
-time.sleep(2)
-quit()
-# really, that's all that is in this script. lol.
-# this is just to make it so the bot.py file can launch again
